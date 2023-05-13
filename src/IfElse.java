a0d7a0721028219ef24bbd36559660a2f01fcc4e
Kaushik13k <13kaushikk@gmail.com>
2023-05-13 21:22:25 +0530

    added the ternary operator

--- src/IfElse.java
+++ src/IfElse.java
@@ -1,14 +1,18 @@
 public class IfElse {
     public static void main(String[] args){
-        String name = "myName";
+        String userName = "myName";
+        int age = 20;
         boolean isHuman = true;
         int battery = 60;
         boolean isEmail = true;
         boolean isPhoneNumber = false;
+        boolean isAge = age >= 18? true: false;
+        String isAgeMessage = age >= 18? "Is allowd to use the social-media": "Is NOT allowd to use the social-media";
+
 
         if (isHuman){
             System.out.println("Verified not a robot!");
-            System.out.println("Welcome " + name);
+            System.out.println("Welcome " + userName);
         }
 
         // Logical AND operator
@@ -20,5 +24,11 @@ public class IfElse {
         if (isEmail || isPhoneNumber){
             System.out.println("Recovery method is set!");
         }
+
+        // Ternary Operator
+        if (isAge){
+            System.out.println(isAgeMessage);
+        }
+
     }
 }
