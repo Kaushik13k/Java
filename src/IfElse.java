8e3a384e32862c3c24986a2c7e109eb1bc4f31d8
Kaushik13k <13kaushikk@gmail.com>
2023-05-20 08:56:04 +0530

    added if-else case

--- src/IfElse.java
+++ src/IfElse.java
@@ -1,33 +1,42 @@
 public class IfElse {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String userName = "myName";
         int age = 20;
         boolean isHuman = true;
         int battery = 60;
         boolean isEmail = true;
         boolean isPhoneNumber = false;
-        boolean isAge = age >= 18? true: false;
-        String isAgeMessage = age >= 18? "Is allowd to use the social-media": "Is NOT allowd to use the social-media";
+        boolean isAge = age >= 18;
+        String isAgeMessage = age >= 18 ? "Is allowd to use the social-media" : "Is NOT allowd to use the social-media";
 
 
-        if (isHuman){
+        if (isHuman) {
             System.out.println("Verified not a robot!");
             System.out.println("Welcome " + userName);
         }
 
         // Logical AND operator
-        if ((battery >= 30) && isHuman){
+        if ((battery >= 30) && isHuman) {
             System.out.println("Logged in!");
         }
 
         // Logical OR operator
-        if (isEmail || isPhoneNumber){
+        if (isEmail || isPhoneNumber) {
             System.out.println("Recovery method is set!");
         }
 
         // Ternary Operator
-        if (isAge){
+        if (isAge) {
             System.out.println(isAgeMessage);
+        }
+
+        // if else
+        if (age > 18) {
+            System.out.println("Eligible to create account");
+        } else if (age > 15) {
+            System.out.println("Two more years");
+        } else {
+            System.out.println("I'm Sorry!");
         }
 
     }
